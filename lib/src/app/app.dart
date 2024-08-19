c5e9d348fccc90d00b4218ce3535453145899e4f
Denis Grudinin <denisgrudininworks@gmail.com>
2024-08-19 14:35:18 +0300

    upd: refactoring phone input code using bloc

--- lib/src/app/app.dart
+++ lib/src/app/app.dart
@@ -1,7 +1,9 @@
 import 'package:ecommerce_app/core/localization/localization.dart';
 import 'package:ecommerce_app/core/theme/theme.dart';
 import 'package:ecommerce_app/src/app/router/router.dart';
+import 'package:ecommerce_app/src/features/login/bloc/enter_phone_page/bloc/phone_input_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 final AppRouter appRouter = AppRouter();
 
@@ -10,13 +12,20 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      title: 'Ecommerce App',
-      theme: AppTheme.themeData,
-      debugShowCheckedModeBanner: false,
-      localizationsDelegates: AppLocalizations.localizationsDelegates,
-      supportedLocales: AppLocalizations.supportedLocales,
-      routerConfig: appRouter.config(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (_) => PhoneInputBloc(),
+        ),
+      ],
+      child: MaterialApp.router(
+        title: 'Ecommerce App',
+        theme: AppTheme.themeData,
+        debugShowCheckedModeBanner: false,
+        localizationsDelegates: AppLocalizations.localizationsDelegates,
+        supportedLocales: AppLocalizations.supportedLocales,
+        routerConfig: appRouter.config(),
+      ),
     );
   }
 }
