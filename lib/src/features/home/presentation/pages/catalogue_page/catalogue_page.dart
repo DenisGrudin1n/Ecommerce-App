c3f22706f5945fc337f48c68a882f297b515bb62
Denis Grudinin <denisgrudininworks@gmail.com>
2024-09-11 16:59:07 +0300

    feat: pull to refresh feature added to home and catalogue pages

--- lib/src/features/home/presentation/pages/catalogue_page/catalogue_page.dart
+++ lib/src/features/home/presentation/pages/catalogue_page/catalogue_page.dart
@@ -1,6 +1,8 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:ecommerce_app/core/theme/colors.dart';
+import 'package:ecommerce_app/core/widgets/refreshable_scroll_view.dart';
 import 'package:ecommerce_app/src/features/home/presentation/pages/catalogue_page/bloc/catalogue_bloc.dart';
+import 'package:ecommerce_app/src/features/home/presentation/pages/catalogue_page/bloc/catalogue_event.dart';
 import 'package:ecommerce_app/src/features/home/presentation/pages/catalogue_page/widgets/catalogue_appbar.dart';
 import 'package:ecommerce_app/src/features/home/presentation/pages/catalogue_page/widgets/catalogue_list.dart';
 import 'package:ecommerce_app/src/features/home/presentation/pages/catalogue_page/widgets/catalogue_searchbar.dart';
@@ -8,6 +10,7 @@ import 'package:ecommerce_app/src/repositories/database/database_repository.dart
 import 'package:ecommerce_app/src/repositories/storage/storage_repository.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
 
 @RoutePage()
 class CataloguePage extends StatelessWidget {
@@ -22,19 +25,39 @@ class CataloguePage extends StatelessWidget {
       ),
       child: Scaffold(
         backgroundColor: AppColors.lightGreyColor,
-        body: ListView(
-          padding: EdgeInsets.zero,
-          children: const [
+        body: Column(
+          children: [
             // AppBar & SearchBar
-            Stack(
+            const Stack(
               children: [
                 CatalogueAppbar(),
                 CatalogueSearchBar(),
               ],
             ),
+            Expanded(
+              child: Builder(
+                builder: (BuildContext context) {
+                  final refreshController = RefreshController();
 
-            // Catalogue List
-            CatalogueList(),
+                  return RefreshableScrollView(
+                    refreshController: refreshController,
+                    onRefresh: () async {
+                      // Dispatch events to refresh the data
+                      context.read<CatalogueBloc>().add(LoadCatalogueEvent());
+                      // Complete the refresh
+                      refreshController.refreshCompleted();
+                    },
+                    child: ListView(
+                      padding: EdgeInsets.zero,
+                      children: const [
+                        // Catalogue List
+                        CatalogueList(),
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ),
           ],
         ),
       ),
--- lib/src/features/home/presentation/pages/home_page/home_page.dart
+++ lib/src/features/home/presentation/pages/home_page/home_page.dart
@@ -2,7 +2,9 @@ import 'package:auto_route/auto_route.dart';
 import 'package:ecommerce_app/core/l10n/l10n.dart';
 import 'package:ecommerce_app/core/theme/colors.dart';
 import 'package:ecommerce_app/core/theme/text_styles.dart';
+import 'package:ecommerce_app/core/widgets/refreshable_scroll_view.dart';
 import 'package:ecommerce_app/src/features/home/presentation/pages/home_page/bloc/home_bloc.dart';
+import 'package:ecommerce_app/src/features/home/presentation/pages/home_page/bloc/home_event.dart';
 import 'package:ecommerce_app/src/features/home/presentation/pages/home_page/widgets/catalogue_section.dart';
 import 'package:ecommerce_app/src/features/home/presentation/pages/home_page/widgets/fashion_sale_section.dart';
 import 'package:ecommerce_app/src/features/home/presentation/pages/home_page/widgets/featured_section.dart';
@@ -12,6 +14,7 @@ import 'package:ecommerce_app/src/repositories/database/database_repository.dart
 import 'package:ecommerce_app/src/repositories/storage/storage_repository.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
 
 @RoutePage()
 class HomePage extends StatelessWidget {
@@ -23,55 +26,82 @@ class HomePage extends StatelessWidget {
       create: (context) => HomeBloc(
         storageRepository: context.read<StorageRepository>(),
         firestoreRepository: context.read<DatabaseRepository>(),
-      ),
+      )
+        ..add(const LoadFeaturedProductsEvent(''))
+        ..add(LoadHomeCatalogueEvent())
+        ..add(LoadFashionSaleImagesEvent()),
       child: Scaffold(
         backgroundColor: AppColors.lightGreyColor,
-        body: CustomScrollView(
-          slivers: [
-            // AppBar & SearchBar
-            const SliverToBoxAdapter(
-              child: Stack(
-                children: [
-                  HomeAppbar(),
-                  HomeSearchBar(),
-                ],
-              ),
-            ),
+        body: Builder(
+          builder: (BuildContext context) {
+            final refreshController = RefreshController();
 
-            const SliverPadding(padding: EdgeInsets.only(top: 20)),
-
-            // Fashion Sale Section
-            const SliverToBoxAdapter(
-              child: FashionSaleSection(),
-            ),
-
-            const SliverPadding(padding: EdgeInsets.only(top: 20)),
-
-            // Catalogue Section
-            const SliverToBoxAdapter(
-              child: CatalogueSection(),
-            ),
-
-            const SliverPadding(padding: EdgeInsets.only(top: 20)),
-
-            SliverToBoxAdapter(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16),
-                child: Text(
-                  context.localization.homePageFeaturedText,
-                  style: HomePageTextStyles.homePageFeaturedTextStyle,
+            return CustomScrollView(
+              slivers: [
+                // AppBar & SearchBar
+                const SliverToBoxAdapter(
+                  child: Stack(
+                    children: [
+                      HomeAppbar(),
+                      HomeSearchBar(),
+                    ],
+                  ),
                 ),
-              ),
-            ),
 
-            const SliverPadding(padding: EdgeInsets.only(top: 16)),
+                const SliverPadding(padding: EdgeInsets.only(top: 20)),
+                SliverFillRemaining(
+                  child: RefreshableScrollView(
+                    refreshController: refreshController,
+                    onRefresh: () async {
+                      // Dispatch events to refresh the data
+                      context
+                          .read<HomeBloc>()
+                          .add(const LoadFeaturedProductsEvent(''));
+                      context.read<HomeBloc>().add(LoadHomeCatalogueEvent());
+                      context
+                          .read<HomeBloc>()
+                          .add(LoadFashionSaleImagesEvent());
+                      // Complete the refresh
+                      refreshController.refreshCompleted();
+                    },
+                    slivers: [
+                      // Fashion Sale Section
+                      const SliverToBoxAdapter(
+                        child: FashionSaleSection(),
+                      ),
 
-            // Featured Section as SliverGrid
-            const SliverPadding(
-              padding: EdgeInsets.symmetric(horizontal: 16),
-              sliver: FeaturedSection(),
-            ),
-          ],
+                      const SliverPadding(padding: EdgeInsets.only(top: 20)),
+
+                      // Catalogue Section
+                      const SliverToBoxAdapter(
+                        child: CatalogueSection(),
+                      ),
+
+                      const SliverPadding(padding: EdgeInsets.only(top: 20)),
+
+                      SliverToBoxAdapter(
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 16),
+                          child: Text(
+                            context.localization.homePageFeaturedText,
+                            style: HomePageTextStyles.homePageFeaturedTextStyle,
+                          ),
+                        ),
+                      ),
+
+                      const SliverPadding(padding: EdgeInsets.only(top: 16)),
+
+                      // Featured Section as SliverGrid
+                      const SliverPadding(
+                        padding: EdgeInsets.symmetric(horizontal: 16),
+                        sliver: FeaturedSection(),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            );
+          },
         ),
       ),
     );
